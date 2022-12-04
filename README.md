86eed4290d831e3352d796d235164666cec2bb4b
RamiroGhilino <ghilinoramiro@gmail.com>
2022-12-04 16:27:32 -0300

    commit para empezar CI/CD

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 Para levantar el BackEnd utilizar el comando `node index.js` 
 
 **Nota**: Este pryoecto utiliza el puerto 5050.
+
+## Respecto de Redis
+
+Para levantar redis utilizo un docker-compose.
+
+Para levatarlo uso el comando `docker-compose up -d` 
--- index.js
+++ index.js
@@ -2,20 +2,92 @@ const express = require('express');
 const app = express();
 const port = 5050;
 
+// Redis setup
+const redis = require('redis');
+
+//6379 es el puerto al que va a buscar la base de datos, usualmente esa es por defecto pero la aclaro por si acaso
+const redisClient = redis.createClient(6379);
+
+// intentar conexión
+(async () => {
+    await redisClient.connect();
+})();
+  
+console.log("Connecting to the Redis");
+  
+redisClient.on("ready", () => {
+    console.log("Connected!");
+});
+  
+redisClient.on("error", (err) => {
+    console.log("Error in the Connection");
+});
+
+
 app.use(express.json());
 
 let count = 0;
 
+
+app.get('/', (req,res) => {
+    (async () => {
+        let count = await redisClient.get('count');
+        if (count === null) {
+            await redisClient.set('count', 0);
+            res.send({'value': 0});
+            return;
+        }
+        console.log(count);
+        res.send({'value': count})
+    })()
+})
+
 app.get('/add', (req, res) => {
-    count++;
-    res.send({'value': count})
+    (async () => {
+        let count = await redisClient.get('count');
+        if (count === null) {
+            await redisClient.set('count', 1);
+            res.send({'value': 1});
+            return;
+        }
+        console.log(count);
+        count++;
+        await redisClient.set('count', count);
+        res.send({'value': count})
+    })()
 })
 
 app.get('/sub', (req, res) => {
-    count--;
-    res.send({'value': count})
+    (async () => {
+        let count = await redisClient.get('count');
+        if (count === null) {
+            await redisClient.set('count', -1);
+            res.send({'value': -1});
+            return;
+        }
+        console.log(count);
+        count--;
+        await redisClient.set('count', count);
+        res.send({'value': count})
+    })()
+})
+
+app.get('/restart', (req, res) => {
+    (async () => {
+        let count = await redisClient.get('count');
+        if (count === null) {
+            await redisClient.set('count', 0);
+            res.send({'value': 0});
+            return;
+        }
+        console.log(count);
+        count = 0;
+        await redisClient.set('count', count);
+        res.send({'value': count})
+    })()
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
